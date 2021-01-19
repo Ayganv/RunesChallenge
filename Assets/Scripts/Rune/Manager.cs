a918674b3afe907d06aaff20001c1ea03c33584a
Jackoberto <jackoberto01@gmail.com>
2021-01-19 11:38:08 +0100

    Makes purchase ready to use

--- Assets/Scripts/Rune/Manager.cs
+++ Assets/Scripts/Rune/Manager.cs
@@ -6,6 +6,16 @@ namespace Rune
 {
     public class Manager : MonoBehaviour, IRuneDataProvider
     {
+        public Config config;
+        public Purchase purchase;
+        
+        public void DoPurchase()
+        {
+            foreach (var data in purchase.PurchaseRandomRunes(4, Rarity.Common, config.runeDatas))
+            {
+               
+            }
+        }
         public IEnumerable<Data> ReceiveData()
         {
             throw new NotImplementedException();
--- Assets/Scripts/Rune/Purchase.cs
+++ Assets/Scripts/Rune/Purchase.cs
@@ -1,9 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace Rune
 {
     public class Purchase : MonoBehaviour
     {
-    
+        public IEnumerable<Data> PurchaseRandomRunes(int amount, Rarity rarity, IEnumerable<Data> datas)
+        {
+            var possibleDatas = datas.Where(data => data.Rarity == rarity).ToList();
+            for (var i = 0; i < amount; i++)
+            {
+                var randomNum = Random.Range(0, possibleDatas.Count);
+                yield return possibleDatas[randomNum];
+                possibleDatas[randomNum].Amount++;
+            }
+        }
     }
 }
